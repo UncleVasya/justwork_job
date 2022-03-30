eddedb4826251aea64d06c7d122834e40b5cf2ce
UncleVasya <unclevasyaai@gmail.com>
2022-03-30 22:11:33 +0300

    api: don't need nested info on pages list

--- app/pages/serializers.py
+++ app/pages/serializers.py
@@ -1,3 +1,4 @@
+from action_serializer import ModelActionSerializer
 from rest_framework import serializers
 from rest_polymorphic.serializers import PolymorphicSerializer
 from app.pages.models import TextPiece, AudioPiece, VideoPiece, Page
@@ -32,9 +33,11 @@ class CPiecePolymorphicSerializer(PolymorphicSerializer):
     }
 
 
-class PageSerializer(serializers.ModelSerializer):
+class PageSerializer(ModelActionSerializer, serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()  # have to include this explicitly for some reason
     pieces = CPiecePolymorphicSerializer(many=True)
 
     class Meta:
         model = Page
         fields = '__all__'
+        action_fields = {'list': {'fields': ('id', 'title', 'url',)}}
