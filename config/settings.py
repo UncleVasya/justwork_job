f212ab4f1edfa6ee598e83b123056f5046c013d9
UncleVasya <unclevasyaai@gmail.com>
2022-03-31 00:26:03 +0300

    added celery, redis and celery-flower

--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50
 }
+
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_BROKER', 'redis://redis:6379/0')
