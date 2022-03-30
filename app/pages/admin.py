8122ac6c8bee7d1cf0903c324b69a373522a2116
UncleVasya <unclevasyaai@gmail.com>
2022-03-30 17:08:47 +0300

    Page model and ContentPiece model

--- app/pages/admin.py
+++ app/pages/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin, \
+    StackedPolymorphicInline, PolymorphicInlineSupportMixin
+from app.pages.models import ContentPiece, TextPiece, AudioPiece, VideoPiece, Page
 
-# Register your models here.
+
+class BaseContentPiece(PolymorphicChildModelAdmin):
+    base_model = ContentPiece
+
+
+class TextPieceAdmin(BaseContentPiece):
+    base_model = TextPiece
+
+
+class AudioPieceAdmin(BaseContentPiece):
+    base_model = AudioPiece
+
+
+class VideoPieceAdmin(BaseContentPiece):
+    base_model = VideoPiece
+
+
+class ContentPieceAdmin(PolymorphicParentModelAdmin):
+    base_model = ContentPiece
+    child_models = (TextPiece, AudioPiece, VideoPiece)
+
+
+class ContentPieceInline(StackedPolymorphicInline):
+    class TextAdminInline(StackedPolymorphicInline.Child):
+        model = TextPiece
+
+    class AudioAdminInline(StackedPolymorphicInline.Child):
+        model = AudioPiece
+
+    class VideoAdminInline(StackedPolymorphicInline.Child):
+        model = VideoPiece
+
+    model = Page.pieces.through
+    child_inlines = (TextAdminInline, AudioAdminInline, VideoAdminInline)
+
+
+class PageAdmin(PolymorphicInlineSupportMixin, admin.ModelAdmin):
+    inlines = (ContentPieceInline,)
+
+
+admin.site.register(TextPiece, TextPieceAdmin)
+admin.site.register(AudioPiece, AudioPieceAdmin)
+admin.site.register(VideoPiece, VideoPieceAdmin)
+admin.site.register(ContentPiece, ContentPieceAdmin)
+
+admin.site.register(Page, PageAdmin)
--- app/pages/models.py
+++ app/pages/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from polymorphic.models import PolymorphicModel
 
-# Create your models here.
+
+class ContentPiece(PolymorphicModel):
+    title = models.CharField(max_length=255)
+    counter = models.PositiveIntegerField()
+
+
+class TextPiece(ContentPiece):
+    text = models.TextField()
+
+
+class AudioPiece(ContentPiece):
+    bitrate = models.PositiveSmallIntegerField()
+
+
+class VideoPiece(ContentPiece):
+    video_url = models.URLField()
+    subtitles_url = models.URLField()
+
+
+class Page(models.Model):
+    pieces = models.ManyToManyField(ContentPiece)
+
